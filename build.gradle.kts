0d30162b316dcac2be35203909baad7e6f73624a
range79 <darkrange006@gmail.com>
2026-03-24 15:36:07 +0400

    micronaut added

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.25"
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
+    id("io.micronaut.application") version "4.6.2"
+    id("com.gradleup.shadow") version "8.3.9"
+    id("io.micronaut.test-resources") version "4.6.2"
+    id("io.micronaut.aot") version "4.6.2"
+}
+
+version = "0.1"
+group = "com.range"
+
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    ksp("io.micronaut.data:micronaut-data-processor")
+
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.slf4j:slf4j-simple")
+    testImplementation("io.micronaut:micronaut-http-client")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+
+application {
+    mainClass = "com.range.ApplicationKt"
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+}
+
+
+
+
+graalvmNative.toolchainDetection = false
+
+
+
+
+
+micronaut {
+    runtime("NONE")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.range.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = false
+        replaceLogbackXml = true
+    }
+
+}
+
+
+tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
+    jdkVersion = "21"
+}
+
+
+
+
+
+
+
---
+++ src/main/kotlin/com/range/Application.kt
@@ -0,0 +1,9 @@
+package com.range
+
+import io.micronaut.runtime.Micronaut
+import io.micronaut.runtime.Micronaut.run
+
+
+fun main(args: Array<String>) {
+    run(*args)
+}
